976f304fe1d4c17c87180f655e544a778fbfeddd
penekhun <penekhun@gmail.com>
2023-01-27 13:15:56 +0900

    Initial commit

---
+++ src/main/java/com/sch/shift3/Shift3Application.java
@@ -0,0 +1,13 @@
+package com.sch.shift3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shift3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shift3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sch/shift3/Shift3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sch.shift3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shift3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
