0e3391cf0127aea39f27a1c737980e7df09115bf
penekhun <penekhun@gmail.com>
2023-02-04 00:40:16 +0900

    헤더 수정

---
+++ src/main/java/com/sch/shift3/config/CustomUserDetails.java
@@ -0,0 +1,25 @@
+//package com.sch.shift3.config;
+//
+//import com.sch.shift3.user.entity.Account;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//public class CustomUserDetails extends User implements UserDetails {
+//
+//    private String name;
+//
+//    public CustomUserDetails(Account account, String name) {
+//        super(account);
+//        this.name = name;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    // Override methods from UserDetails
+//    // ...
+//
+//    // Add additional methods as needed
+//    // ...
+//}
--- src/main/java/com/sch/shift3/config/SecurityUser.java
+++ src/main/java/com/sch/shift3/config/SecurityUser.java
@@ -1,14 +1,55 @@
 package com.sch.shift3.config;
 
 import com.sch.shift3.user.entity.Account;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class SecurityUser extends User {
+import java.util.Collection;
+
+public class SecurityUser implements UserDetails {
     private Account account;
 
     public SecurityUser(Account account) {
-        super(account.getUsername(), account.getPassword(), AuthorityUtils.createAuthorityList(account.getRole().toString()));
+//        super(account.getUsername(), account.getPassword(), AuthorityUtils.createAuthorityList(account.getRole().toString()));
         this.account = account;
+    }
+    public String getName() {
+        return account.getName();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.createAuthorityList(account.getRole().toString());
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !account.getBan();
     }
 }
